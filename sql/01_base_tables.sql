015bef9c2a572655d024ac8163c0046e07a06586
Andrzej Hołda <100772902+andrzejholda@users.noreply.github.com>
2026-01-30 12:53:17 +0100

    Update 01_base_tables.sql

--- sql/01_base_tables.sql
+++ sql/01_base_tables.sql
@@ -1,9 +1,19 @@
+-- INPUT:
+-- patient_visits
+-- dim_clinics
+-- dim_procedures
+--
+-- OUTPUT:
+-- fact_patient_visits
+--
+-- Purpose:
+-- Prepare clean visit-level fact table enriched with analytical dimensions
+
 CREATE OR REPLACE VIEW fact_patient_visits AS
 SELECT
     v.patient_id,
     v.visit_date,
     v.revenue,
-    v.status,
 
     c.clinic_id,
     c.clinic_name,
