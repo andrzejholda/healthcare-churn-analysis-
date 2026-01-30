53190fb8a4a918ea59d32e14241e562f140b015c
Andrzej Hołda <100772902+andrzejholda@users.noreply.github.com>
2026-01-30 12:54:00 +0100

    Update 02_monthly_aggregation.sql

--- sql/02_monthly_aggregation.sql
+++ sql/02_monthly_aggregation.sql
@@ -1,16 +1,19 @@
--- INPUT: patient_visits
--- OUTPUT: fact_monthly_patient_activity
--- Grain: patient_id + month
+-- INPUT:
+-- fact_patient_visits
+--
+-- OUTPUT:
+-- fact_monthly_patient_activity
+--
+-- Grain:
+-- patient_id + month + clinic_id + procedure_id
 
-WITH base AS (
-    SELECT
-        patient_id,
-        DATE_TRUNC('month', visit_date) AS month,
-        SUM(revenue) AS revenue
-    FROM patient_visits
-    WHERE status = 'Completed'
-    GROUP BY 1,2
-)
-
-SELECT *
-FROM base;
+CREATE OR REPLACE VIEW fact_monthly_patient_activity AS
+SELECT
+    patient_id,
+    DATE_TRUNC('month', visit_date) AS month,
+    clinic_id,
+    procedure_id,
+    SUM(revenue) AS revenue,
+    COUNT(*) AS visits_count
+FROM fact_patient_visits
+GROUP BY 1,2,3,4;
