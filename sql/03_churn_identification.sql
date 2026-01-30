00c9550676cce9694951d324228c3ddd81baa652
Andrzej Hołda <100772902+andrzejholda@users.noreply.github.com>
2026-01-30 11:50:04 +0100

    Update 03_churn_identification.sql

--- sql/03_churn_identification.sql
+++ sql/03_churn_identification.sql
@@ -1,1 +1,32 @@
+-- INPUT: fact_monthly_patient_activity
+-- OUTPUT: churn_flagged_activity
+-- Churn definition:
+-- A patient is considered churned in month M
+-- if they are not active in month M+1
+
+WITH activity AS (
+    SELECT
+        patient_id,
+        month,
+        revenue,
+        LEAD(month) OVER (
+            PARTITION BY patient_id
+            ORDER BY month
+        ) AS next_month
+    FROM fact_monthly_patient_activity
+),
+
+churn_flag AS (
+    SELECT
+        *,
+        CASE
+            WHEN next_month IS NULL THEN 1
+            WHEN next_month > month + INTERVAL '1 month' THEN 1
+            ELSE 0
+        END AS is_churn
+    FROM activity
+)
+
+SELECT *
+FROM churn_flag;
 
