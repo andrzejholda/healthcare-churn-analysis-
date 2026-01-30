b77193bb2f364cae8b1c5b5e7bc6e3d0691a3335
Andrzej Hołda <100772902+andrzejholda@users.noreply.github.com>
2026-01-30 11:27:17 +0100

    Update 01_base_tables.sql

--- sql/01_base_tables.sql
+++ sql/01_base_tables.sql
@@ -1,1 +1,9 @@
-
+-- Base fact table for patient visits
+SELECT
+    c.patient_id,
+    c.visit_date,
+    p.clinic_id,
+    c.revenue,
+    c.status
+FROM clinic_visits c JOIN  public_clinics p ON c.patient_id = p.patient_id
+WHERE status = 'Completed';
